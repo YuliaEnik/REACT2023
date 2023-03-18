b156012e6f3e12739f5ca22097dd52802d6de2c8
Yuliya Aleinik <aleinikya@mail.ru>
2023-03-18 21:14:31 +0300

    feat: add cards on home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,12 +1,41 @@
 import './home.scss';
 import React from 'react';
 import { Search } from '../../Components/search';
+import { Card } from '../../Components/card/Сard';
+import { data } from '../../Data/data';
+import { IData } from '../../Data/data';
 
-class Home extends React.Component {
+type IProps = {};
+class Home extends React.Component<IProps, IData[]> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = [{
+      author: '',
+      name: '',
+      year: '',
+      imageNum: 0,
+   }]
+  }
+
   render() {
     return(
     <div className='cards-page'>
     <Search></Search>
+   <div className='cards-wrapper'>
+        {!data ? (
+          <div>No Data</div>
+        ) : (
+          data.map((cardData: IData) => (
+            <Card
+              imageNum={cardData.imageNum}
+              author={cardData.author}
+              name={cardData.name}
+              year={cardData.year}
+              key={cardData.imageNum}
+            ></Card>
+          ))
+        )}
+        </div> 
     </div>
     )
   };
