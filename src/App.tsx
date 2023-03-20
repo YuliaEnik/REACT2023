eaf9bed9caf4790f2abdd6971bc43ad5620ec8bf
Yuliya Aleinik <aleinikya@mail.ru>
2023-03-20 21:32:39 +0300

    feat: add redirect in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { NotFound } from "./Pages/NotFound";
 import { AboutUs } from "./Pages/AboutUs";
@@ -11,7 +11,8 @@ function App(): JSX.Element {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Route>
       </Routes>
     </>
