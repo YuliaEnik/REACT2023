8fbe7473bcb881451cb34cec585f32255ac1aea7
Yuliya Aleinik <aleinikya@mail.ru>
2023-03-27 16:30:37 +0300

    feat: finished validation

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,154 +1,235 @@
 import React from "react";
 import { Button } from "../Button";
+import {
+  IErrors,
+  IFormState,
+  Props,
+  IData,
+  FormData,
+  FormProps,
+} from "./types";
 //import { CardForm } from "../../Components/Card-form";
 import "./form.scss";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type Props = {};
-interface IData {
-  name: string | null;
-  birthday: string | null;
-  country: string | null;
-  gender: string | boolean | null;
-  agree: string | boolean | null;
-  file: string | boolean | null;
-}
-class Form extends React.Component<Props, IData> {
+class Form extends React.Component<FormProps, IFormState> {
+  formRef: React.RefObject<HTMLFormElement>;
   nameInput: React.RefObject<HTMLInputElement>;
   birthdayInput: React.RefObject<HTMLInputElement>;
   countryInput: React.RefObject<HTMLSelectElement>;
-  genderInput: React.RefObject<HTMLInputElement>;
+  genderInputM: React.RefObject<HTMLInputElement>;
+  genderInputF: React.RefObject<HTMLInputElement>;
   agreeInput: React.RefObject<HTMLInputElement>;
   fileInput: React.RefObject<HTMLInputElement>;
 
-  constructor(props: Props) {
+  constructor(props: FormProps) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formRef = React.createRef();
     this.nameInput = React.createRef();
     this.birthdayInput = React.createRef();
     this.countryInput = React.createRef();
-    this.genderInput = React.createRef();
+    this.genderInputM = React.createRef();
+    this.genderInputF = React.createRef();
     this.agreeInput = React.createRef();
     this.fileInput = React.createRef();
 
     this.state = {
-      name: "",
-      birthday: "",
-      country: "",
-      gender: "",
-      agree: "",
-      file: "",
+      message: "",
+      errors: {
+        nameError: "",
+        dateError: "",
+        countryError: "",
+        sexError: "",
+        agreeError: "",
+        fileError: "",
+      },
     };
-    /* this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this); */
   }
 
-  onChangeHandler = (e: React.FormEvent<HTMLFormElement>) => {
+  handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    /* this.state = {
-      name: this.nameInput.current!.value,
-      birthday: this.birthdayInput.current!.value,
-      country: this.countryInput.current!.value,
-      gender: this.genderInput.current!.value,
-      agree: this.agreeInput.current!.value,
-      file: this.fileInput.current!.value,
-    }; */
-  };
+    if (this.validation()) {
+      const data: IData = {};
+      //data.id = uuidv4();
+      if (this.nameInput.current) {
+        data.name = this.nameInput.current.value;
+      }
+      if (this.birthdayInput.current) {
+        data.birthday = this.birthdayInput.current.value;
+      }
+      if (this.countryInput.current) {
+        data.country = this.countryInput.current.value;
+      }
+      if (this.genderInputM.current) {
+        data.gender = this.genderInputM.current.checked ? "male" : "female";
+      }
+      this.setState({ message: "Information has been saved" });
+      this.addCard(data);
+      setTimeout(() => {
+        this.setState({ message: "" });
+      }, 2000);
 
-  validation = () => {
-    //const errors = {};
+      this.resetForm();
+    }
+  }
+  validation() {
     let isValid = true;
+    const errors: IErrors = {
+      nameError: "",
+      dateError: "",
+      countryError: "",
+      sexError: "",
+      agreeError: "",
+      fileError: "",
+    };
 
     if (
       !this.nameInput.current?.value ||
-      !/^[a-zA-Zа-яФ-Я]+$/.test(this.nameInput.current.value)
+      !/^[a-zA-Zа-яА-Я]+$/.test(this.nameInput.current.value)
     ) {
       isValid = false;
-      //errors.name = "The name should contain only letters";
+      errors.nameError = "The name should contain 1 or more letters";
+    }
+    if (!this.birthdayInput.current?.value) {
+      isValid = false;
+      errors.dateError = "Choose date";
+    } else if (new Date(this.birthdayInput.current?.value) > new Date()) {
+      isValid = false;
+      errors.dateError = "Date of Birth cannot be more than today's date";
     }
     if (!this.countryInput.current?.value) {
       isValid = false;
-      //errors.country = "Enter country";
+      errors.countryError = "Enter country";
+    }
+    if (
+      !this.genderInputM.current?.checked &&
+      !this.genderInputF.current?.checked
+    ) {
+      isValid = false;
+      errors.sexError = "Choose your gender";
+    }
+    if (!this.fileInput.current?.value) {
+      isValid = false;
+      errors.fileError = "Choosse a file";
     }
     if (!this.agreeInput.current?.checked) {
       isValid = false;
-      //errors.agree = "You need to agree";
+      errors.agreeError = "You need to agree";
     }
     this.setState({
-      //errors: errors,
+      errors: errors,
     });
     return isValid;
-  };
+  }
+
+  addCard(card: IData) {
+    this.props.refreshCards(card);
+  }
+
+  resetForm() {
+    this.formRef.current?.reset();
+  }
 
   render() {
     return (
       <>
-        <form className="form-wrapper">
-          <label className="form-line">
-            Name:
-            <input
-              type="input"
-              className="input"
-              ref={this.nameInput}
-              //valid={this.state.nameInput}
-              //onChange={this.onChangeHandler}
-            />
-          </label>
-          <label className="form-line">
-            Birthday:
-            <input
-              type="date"
-              className="input"
-              //ref={this.Birthday}
-              //errorMessage="Choose date"
-              // valid={this.state.date}
-              //onChange={this.onChangeHandler}
-            />
-          </label>
-          <label className="form-line">
-            Country:
-            <select
-              className="input"
-              //ref="country"
-              //errorMessage="Choose country"
-              //valid={this.state.country}
-              //onChange={this.onChangeHandler}
-            >
-              <option value=""> </option>
-              <option value="Belarus"> Belarus </option>
-              <option value="USA"> USA </option>
-              <option value="Poland"> Poland </option>
-              <option value="Germany"> Germany </option>
-            </select>
-          </label>
-          <label className="form-line">
-            Male
-            <input type="radio" name="male-female" value="male" />
-          </label>
-          <label className="form-line">
-            Female
-            <input type="radio" name="male-female" value="female" />
-          </label>
-          <label className="form-line">
-            Choose file:
-            <input
-              type="file"
-              //ref="file"
-              //errorMessage="Choose file"
-              //valid={this.state.file}
-              //onChange={this.onChangeHandler}
-            />
-          </label>
-          <label className="form-line">
-            I agree:
-            <input
-              type="checkbox"
-              //ref="agree"
-              //errorMessage="should agree "
-              //valid={this.state.agree}
-              //onChange={this.onChangeHandler}
-            />
-          </label>
+        <form
+          className="form-wrapper"
+          onSubmit={this.handleSubmit}
+          ref={this.formRef}
+        >
+          <div className="input-wrapper">
+            <label className="form-line">
+              Name:
+              <input type="input" className="input" ref={this.nameInput} />
+            </label>
+            {this.state.errors.nameError ? (
+              <p className="error">{this.state.errors.nameError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label className="form-line">
+              Birthday:
+              <input type="date" className="input" ref={this.birthdayInput} />
+            </label>
+            {this.state.errors.dateError ? (
+              <p className="error">{this.state.errors.dateError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label className="form-line">
+              Country:
+              <select className="input" ref={this.countryInput}>
+                <option value=""> </option>
+                <option value="Belarus"> Belarus </option>
+                <option value="USA"> USA </option>
+                <option value="Poland"> Poland </option>
+                <option value="Germany"> Germany </option>
+              </select>
+            </label>
+            {this.state.errors.countryError ? (
+              <p className="error">{this.state.errors.countryError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label className="form-line">
+              Male
+              <input
+                type="radio"
+                name="gender"
+                ref={this.genderInputM}
+                value="male"
+              />
+            </label>
+            <label className="form-line">
+              Female
+              <input
+                type="radio"
+                name="gender"
+                ref={this.genderInputF}
+                value="female"
+              />
+            </label>
+            {this.state.errors.sexError ? (
+              <p className="error">{this.state.errors.sexError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label className="form-line">
+              Choose file:
+              <input type="file" ref={this.fileInput} />
+            </label>
+            {this.state.errors.fileError ? (
+              <p className="error">{this.state.errors.fileError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
+          <div className="input-wrapper">
+            <label className="form-line">
+              I agree:
+              <input type="checkbox" ref={this.agreeInput} />
+            </label>
+            {this.state.errors.agreeError ? (
+              <p className="error">{this.state.errors.agreeError}</p>
+            ) : (
+              <br />
+            )}
+          </div>
           <Button>Submit</Button>
+          {this.state.message ? (
+            <p className="form-message">{this.state.message}</p>
+          ) : (
+            <br />
+          )}
         </form>
         {/* {data.map((item: IData, index: number) => (
           <CardForm
